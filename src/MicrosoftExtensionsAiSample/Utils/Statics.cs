eb30e722eedf25062dfc8b8d815f30f78545234c
Sebastian Jensen <github@tsjdev-apps.de>
2024-11-13 21:57:46 +0100

    Add sample project.

---
+++ src/MicrosoftExtensionsAiSample/Utils/Statics.cs
@@ -0,0 +1,13 @@
+﻿namespace MicrosoftExtensionsAiSample.Utils;
+
+internal static class Statics
+{
+    public const string OllamaKey = "Ollama";
+    public const string OpenAiKey = "OpenAI";
+    public const string AzureOpenAiKey = "Azure OpenAI";
+
+    public const string Gpt4oModelName = "gpt-4o";
+    public const string Gpt4oMiniModelName = "gpt-4o-mini";
+    public const string Gpt4TurboModelName = "gpt-4-turbo";
+    public const string Gpt4ModelName = "gpt-4";
+}
